82362b7ccc4825bfc2b1e2642fa499a12959f8e0
root <root@chasm.localdomain>
2022-10-28 17:14:29 +0300

    table structure and some functions

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 typedef struct {
   char* buffer;
@@ -107,6 +108,7 @@ void execute_statement(Statement* statement) {
   }
 }
 
+//row structure
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 const u_int32_t ID_SIZE = size_of_attribute(Row, id);
 const u_int32_t USERNAME_SIZE = size_of_attribute(Row,username);
@@ -115,6 +117,41 @@ const u_int32_t ID_OFFSET = 0;
 const u_int32_t USERNAME_OFFEST = ID_OFFSET + ID_SIZE;
 const u_int32_t EMAIL_OFFSET = USERNAME_OFFEST + USERNAME_SIZE;
 const u_int32_t ROW_SIZE  = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+//table structure
+const u_int32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const u_int32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE; 
+const u_int32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+void serialize_row(Row* source, void* destination) {
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFEST, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination)  {
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFEST, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_OFFSET);
+}
+
+typedef struct {
+  u_int32_t num_rows;
+  void* pages[TABLE_MAX_PAGES];
+} Table;
+
+void* row_slot(Table* table, u_int32_t row_num) {
+  u_int32_t page_num = row_num / ROWS_PER_PAGE;
+  void* page = table->pages[page_num];
+  if (page == NULL) {
+    // Allocate memory only when we try to access page
+    page = table->pages[page_num] = malloc(PAGE_SIZE);
+  }
+  u_int32_t row_offset = row_num % ROWS_PER_PAGE;
+  u_int32_t byte_offset = row_offset * ROW_SIZE;
+  return page + byte_offset;
+}
 
 int main(int argc, char* argv[] ) {
 

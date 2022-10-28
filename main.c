73c0d9844db81e5aee11461a8bdcfa35b6392389
root <root@chasm.localdomain>
2022-10-28 20:56:21 +0300

    little fix

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ typedef enum {
 } ExecuteResult;
 
 InputBuffer* new_input_buffer() {
-  InputBuffer* input_buffer = malloc(sizeof(input_buffer)); 
+  InputBuffer* input_buffer = malloc(sizeof(InputBuffer)); 
   input_buffer->buffer = NULL;
   input_buffer->buffer_length = 0;
   input_buffer->input_length = 0;

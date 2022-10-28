fef317aa0c162d0166479401bc049307a41a315b
root <root@chasm.localdomain>
2022-10-28 16:25:11 +0300

    some functions + row structure

--- main.c
+++ main.c
@@ -17,7 +17,8 @@ typedef enum  {
 
 typedef enum {
   PREPARE_SUCCES, 
-  PREPARE_UNRECOGNIZED_STATEMENT 
+  PREPARE_UNRECOGNIZED_STATEMENT,
+  PREPARE_SYNTAX_ERROR
 } PrepareResult;
 
 typedef enum {
@@ -25,8 +26,17 @@ typedef enum {
   STATEMENT_SELECT,
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct {
+  u_int32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct {
   StatementType type;
+  Row row_to_insert;
 } Statement;
 
 InputBuffer* new_input_buffer() {
@@ -71,6 +81,12 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
     statement->type = STATEMENT_INSERT;
+    int args_assigned = sscanf(
+      input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+      statement->row_to_insert.username, statement->row_to_insert.email);
+    if (args_assigned < 3) {
+      return PREPARE_SYNTAX_ERROR;
+    }
     return PREPARE_SUCCES;
   }
   if (strncmp(input_buffer->buffer, "select", 6) == 0) {
@@ -90,6 +106,15 @@ void execute_statement(Statement* statement) {
       break;
   }
 }
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+const u_int32_t ID_SIZE = size_of_attribute(Row, id);
+const u_int32_t USERNAME_SIZE = size_of_attribute(Row,username);
+const u_int32_t EMAIL_SIZE = size_of_attribute(Row,email);
+const u_int32_t ID_OFFSET = 0;
+const u_int32_t USERNAME_OFFEST = ID_OFFSET + ID_SIZE;
+const u_int32_t EMAIL_OFFSET = USERNAME_OFFEST + USERNAME_SIZE;
+const u_int32_t ROW_SIZE  = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 int main(int argc, char* argv[] ) {
 
